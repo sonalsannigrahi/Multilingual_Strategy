49e3b744931f87614d5cdd1888ce7ddb9b269715
SANNIGRAHI Sonal <sonal.sannigrahi@inria.fr>
2021-09-05 16:37:52 +0000

    sampled file support

--- train.sh
+++ train.sh
@@ -9,11 +9,11 @@
 export PATH=/opt/homebrew/bin:$PATH
 
 SRCS=(
-    "fi"
     "hi"
-    "et"
-    "ne"
+    "fi"
     "gu"
+    "ne"
+    "et"
 )
 
 TGT=(
@@ -38,10 +38,10 @@ SPM_ENCODE=$SCRIPTS/spm_encode.py
 BPESIZE=32000
 DATA=./multi.en.bpe32k
 
-#mkdir -p "$DATA"
+mkdir -p "$DATA"
 
 # learn BPE with sentencepiece
-TRAIN_FILES=$(for SRC in "${SRCS[@]}"; do echo ./${SRC}-${TGT}.train_${SRC}; echo ./${SRC}-${TGT}.train_${TGT}; done | tr "\n" ",")
+TRAIN_FILES=$(for SRC in "${SRCS[@]}"; do echo ./data/${SRC}/${SRC}-${TGT}.train_${SRC}-sampled; echo ./data/${SRC}/${SRC}-${TGT}.train_${TGT}-sampled; done | tr "\n" ",")
 echo "learning joint BPE over ${TRAIN_FILES}..."
 python "$SPM_TRAIN" \
     --input=$TRAIN_FILES \
@@ -57,10 +57,12 @@ for SRC in "${SRCS[@]}"; do
     python "$SPM_ENCODE" \
         --model "$DATA/sentencepiece.bpe.model" \
         --output_format=piece \
-        --inputs ./${SRC}-${TGT}.train_${SRC} ./${SRC}-${TGT}.train_${TGT} \
-        --outputs ./data/train.bpe.${SRC}-${TGT}.${SRC} ./data/train.bpe.${SRC}-${TGT}.${TGT} \
+        --inputs ./data/${SRC}/${SRC}-${TGT}.train_${SRC}-sampled ./data/${SRC}/${SRC}-${TGT}.train_${TGT}-sampled \
+        --outputs $DATA/train.bpe.${SRC}-${TGT}.${SRC} $DATA/train.bpe.${SRC}-${TGT}.${TGT} \
         --min-len $TRAIN_MINLEN --max-len $TRAIN_MAXLEN
 done
+
+#validation data does not need to be sampled?
 
 #encode valid
 echo "encoding valid with learned BPE..."
@@ -97,26 +99,23 @@ done
 
 
 #BPE MODEL
-#mkdir -p checkpoints/multilingual_transformer
-#
-#CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/multi-en.bpe32k/ \
-#  --encoder-normalize-before --decoder-normalize-before \
-#  --arch transformer --layernorm-embedding \
-#  --task translation_multi_simple_epoch \
-#  --sampling-method "temperature" \
-#  --sampling-temperature 1.5 \
-#  --encoder-langtok "src" \
-#  --decoder-langtok \
-#  --lang-dict "$lang_list" \
-#  --lang-pairs "$lang_pairs" \
-#  --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
-#  --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
-#  --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
-#  --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
-#  --max-tokens 1024 --update-freq 2 \
-#  --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10\
-#  --save-dir checkpoints/multilingual_transformer \
-#  --seed 222 --log-format simple --log-interval 2
+mkdir -p checkpoints/multilingual_transformer
 
-
-
+CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/multi-en.bpe32k/ \
+  --encoder-normalize-before --decoder-normalize-before \
+  --arch transformer --layernorm-embedding \
+  --task translation_multi_simple_epoch \
+  --sampling-method "temperature" \
+  --sampling-temperature 1.5 \
+  --encoder-langtok "src" \
+  --decoder-langtok \
+  --lang-dict "$lang_list" \
+  --lang-pairs "$lang_pairs" \
+  --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
+  --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
+  --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
+  --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
+  --max-tokens 1024 --update-freq 2 \
+  --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10\
+  --save-dir checkpoints/multilingual_transformer \
+  --seed 222 --log-format simple --log-interval 2
