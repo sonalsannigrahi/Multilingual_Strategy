acd9fcc223ed32b3040e311ddd82bb1bc263c8c0
SANNIGRAHI Sonal <sonal.sannigrahi@inria.fr>
2021-09-05 16:42:57 +0000

    fix typos

--- train-byte.sh
+++ train-byte.sh
@@ -95,34 +95,24 @@ done
 #   TRAINING    #
 #################
 
-
-##BPE MODEL
-#mkdir -p checkpoints/multilingual_transformer
-#
-#
-#CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/multi-en.bpe32k/ \
-#  --encoder-normalize-before --decoder-normalize-before \
-#  --arch transformer --layernorm-embedding \
-#  --task translation_multi_simple_epoch \
-#  --sampling-method "temperature" \
-#  --sampling-temperature 1.5 \
-#  --encoder-langtok "src" \
-#  --decoder-langtok \
-#  --lang-dict "$lang_list" \
-#  --lang-pairs "$lang_pairs" \
-#  --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
-#  --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
-#  --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
-#  --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
-#  --max-tokens 1024 --update-freq 2 \
-#  --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10\
-#  --save-dir checkpoints/multilingual_transformer \
-#  --seed 222 --log-format simple --log-interval 2
-
-#CHAR MODEL
+mkdir -p checkpoints/multilingual_transformer_byte
 
 
-
-#BYTE MODEL
-
-
+CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/multi-en.byte32k/ \
+ --encoder-normalize-before --decoder-normalize-before \
+ --arch transformer --layernorm-embedding \
+ --task translation_multi_simple_epoch \
+ --sampling-method "temperature" \
+ --sampling-temperature 1.5 \
+ --encoder-langtok "src" \
+ --decoder-langtok \
+ --lang-dict "$lang_list" \
+ --lang-pairs "$lang_pairs" \
+ --criterion label_smoothed_cross_entropy --label-smoothing 0.2 \
+ --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
+ --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 \
+ --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
+ --max-tokens 1024 --update-freq 2 \
+ --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10\
+ --save-dir checkpoints/multilingual_transformer_byte \
+ --seed 222 --log-format simple --log-interval 2
